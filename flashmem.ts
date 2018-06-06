e535503c4da79f12e564bc37fe895c78bafbdf93
pecaras <comercial@idesoftware.com>
2018-06-06 11:39:11 +0200

    commit 2

--- flashmem.ts
+++ flashmem.ts
@@ -9,8 +9,8 @@ namespace FlashMem {
 	* @param name nom variable
 	* @param valor (un byte)
 	*/
-	//% advanced=true weight=10
-	//% blockId="setByte" block="save byte value"
+	//% advanced=false weight=10
+	//% blockId=setByte block="save %name %value"
 	//% blockExternalInputs=2 weight=90 blockGap=8 shim=FlashMem::setByte
 	export function setByte(name: string, value: number) {	
           FlashMem.fmsetByte(name, value);
@@ -22,11 +22,14 @@ namespace FlashMem {
 	* @param name nom variable
 	* @param valor de variable
 	*/
-	//% advanced=true weight=10
-	//% blockId="getByte" block="get byte value"
+	//% advanced=false weight=10
+	//% blockId=getByte block="get %name"
 	//% blockExternalInputs=1 weight=90 blockGap=8 shim=FlashMem::getByte
 	export function getByte(name: string): number {
 	  return FlashMem.fmgetByte(name);
 	}
 	
 }
+
+
+
